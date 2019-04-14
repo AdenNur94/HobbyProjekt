0f0fe44a8b1540eaddff87c3c2142e3e6bd8163b
Aden <aden.nur@dynabyte.se>
2019-04-14 19:38:00 +0200

    Upload Project

---
+++ ReactShop.Web/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using ReactShop.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReactShop.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        readonly ICheckoutManager checkoutManager;
+
+        public HomeController()
+        {
+            this.checkoutManager = AutoFacHelper.Resolve<ICheckoutManager>();
+        }
+
+        public HomeController(ICheckoutManager checkoutManager)
+        {
+            this.checkoutManager = checkoutManager;
+        }
+
+        public ActionResult Index()
+        {
+            return View(checkoutManager.GetProducts());
+        }
+
+        public ActionResult Cart()
+        {
+            return View(checkoutManager.GetCart());
+        }
+
+        public ActionResult CheckoutSuccess()
+        {
+            return View(checkoutManager.GetCheckoutSummary());
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddToCart(string SKU)
+        {
+            checkoutManager.SaveCart(new Core.DTOs.CartItemDTO
+            {
+                 SKU = SKU,
+                 Quantity = 1
+            });
+            return RedirectToAction("Cart", "Home");
+        }
+    }
+}
+
---
+++ ReactShop.Web/WebApi/CartController.cs
@@ -0,0 +1,52 @@
+﻿using ReactShop.Core;
+using ReactShop.Core.DTOs;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace ReactShop.Web.WebApi
+{
+    public class CartController : ApiController
+    {
+        readonly ICheckoutManager checkoutManager;
+
+        public CartController()
+        {
+            this.checkoutManager = AutoFacHelper.Resolve<ICheckoutManager>();
+        }
+
+        public CartController(ICheckoutManager checkoutManager)
+        {
+            this.checkoutManager = checkoutManager;
+        }
+
+        // POST: api/Cart
+        [OutputCache(NoStore = true)]
+        [ValidateHttpAntiForgeryToken]
+        public CartDTO Post([FromBody]CartItemDTO value)
+        {
+            var cart = checkoutManager.GetCart();
+            var cartItem = cart.CartItems.Where(i => i.SKU == value.SKU).SingleOrDefault();
+            if (cartItem != null)
+             {
+                cartItem.Quantity = value.Quantity;
+                var recalculatedCart = checkoutManager.GetCart(cart.CartItems);
+
+                checkoutManager.SaveCart(cartItem);
+                return recalculatedCart;
+            }
+            else
+            {
+                return cart;
+            }
+        }
+    }
+}
